0233c0fbc5b6d7c0b7ff92d7e1ba1f6fc6617086
Isaac J <duckhunterjr@gmail.com>
2023-01-30 22:53:17 -0800

    fixes error handling and logic tweeks

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 // known bugs:
 // * after submitting, if you enter a blank value, it will display an error.
-## Resolved: empty() will return false w/ empty string, isset() will return true w/ empty string
+## Resolved: line 19 and 42 handle blank value
 // * round result to 2 decimal places creates error
 ## Resolved
 // * pressing convert with blank "Please enter a number" is no longer prompting error
@@ -15,41 +15,40 @@
 //celcius to kelvin - C + 273.15 = K
 //kelvin to celcius - K - 273.15 = C
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['userInput']) !=='' && isset($_POST['tempA']) && isset($_POST['tempB'])) {
+        $userInput = $_POST['userInput'];
+        $tempA = $_POST['tempA'];
+        $tempB = $_POST['tempB'];
 
-if (!empty($_POST['userInput']) && isset($_POST['tempA']) && isset($_POST['tempB'])) {
-    $userInput = $_POST['userInput'];
-    $tempA = $_POST['tempA'];
-    $tempB = $_POST['tempB'];
+        // convert temperature
+        if ($tempA == 'fahr' && $tempB == 'cel') {
+            $result = round(($userInput - 32) * 5/9,2);
+        } elseif ($tempA == 'cel' && $tempB == 'fahr') {
+            $result = round(($userInput * 9/5) + 32,2);
+        } elseif ($tempA == 'fahr' && $tempB == 'kel') {
+            $result = round(($userInput + 459.67) * 5/9,2);
+        } elseif ($tempA == 'kel' && $tempB == 'fahr') {
+            $result = round(($userInput * 9/5) - 459.67,2);
+        } elseif ($tempA == 'cel' && $tempB == 'kel') {
+            $result = round($userInput + 273.15,2);
+        } elseif ($tempA == 'kel' && $tempB == 'cel') {
+            $result = round($userInput - 273.15,2);
+        }
+    }
 
-    // convert temperature
-    if ($tempA == 'fahr' && $tempB == 'cel') {
-        $result = round(($userInput - 32) * 5/9,2);
-    } elseif ($tempA == 'cel' && $tempB == 'fahr') {
-        $result = round(($userInput * 9/5) + 32,2);
-    } elseif ($tempA == 'fahr' && $tempB == 'kel') {
-        $result = round(($userInput + 459.67) * 5/9,2);
-    } elseif ($tempA == 'kel' && $tempB == 'fahr') {
-        $result = round(($userInput * 9/5) - 459.67,2);
-    } elseif ($tempA == 'cel' && $tempB == 'kel') {
-        $result = round($userInput + 273.15,2);
-    } elseif ($tempA == 'kel' && $tempB == 'cel') {
-        $result = round($userInput - 273.15,2);
+    // error handling
+
+    if ($_POST['userInput'] === '') {
+        $result = 'Please enter a number';
+    } elseif (!isset($_POST['tempA'])) {
+        $result = 'Please select a temperature type';
+    } elseif (!isset($_POST['tempB'])) {
+        $result = 'Please select a temperature type to convert to';
+    } elseif ($_POST['tempA'] == $_POST['tempB']) {
+        $result = 'Please select a different temperature type to convert to';
     }
 }
-
-
-// error handling
-
-if (empty($_POST['userInput'])) {
-    $result = 'Please enter a number';
-} elseif (!isset($_POST['tempA'])) {
-    $result = 'Please select a temperature type';
-} elseif (!isset($_POST['tempB'])) {
-    $result = 'Please select a temperature type to convert to';
-} elseif ($_POST['tempA'] == $_POST['tempB']) {
-    $result = 'Please select a different temperature type to convert to';
-}
-
 
 ?>
 
@@ -107,8 +106,7 @@ if (empty($_POST['userInput'])) {
     <div class="result">
     <?php
                 echo "<pre>";
-                echo var_dump([$_POST]);
-                echo var_dump($result);              
+                echo var_dump($_POST);              
                 echo"</pre>";
 
                 if (!isset($result)) {
