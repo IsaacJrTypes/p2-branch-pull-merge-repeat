d9e6af4a15e2844302422f4968ee8a9596e6464f
Isaac J <duckhunterjr@gmail.com>
2023-01-31 15:39:54 -0800

    add css link

--- index.php
+++ index.php
@@ -59,6 +59,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./css/styles.css" type="text/css" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
@@ -105,13 +106,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     <div class="result">
     <?php
-                echo "<pre>";
-                echo var_dump($_POST);              
-                echo"</pre>";
-
-                if (!isset($result)) {
-                    echo 'blank';
-                } else {
+                if (isset($result)) {
                     echo $result;
                 }
 ?>
